93a3924ee282e029b76be18f3d685b56d58204d6
Ojie Precious <peterjohn56464@gmail.com>
2024-01-26 09:43:22 +0100

    example-2

---
+++ bin/example2.dart
@@ -0,0 +1,26 @@
+void main() {
+  // Numbers: int
+  int score = 34;
+  var count = 23;
+  int hexValue = 0xEADEBAEE;
+
+  // Numbers: int
+  double percentage = 89.4;
+  var percent = 82.3333;
+  double exponents = 1.42e5;
+
+  // Strings
+  String name = "HENRY";
+  var company = 'Google';
+
+  // Boolean
+  bool isValid = true;
+  var isAlive = false;
+
+  print(hexValue);
+  print(exponents);
+  print(percent);
+
+  // NOTE: All Data typrs in Dart are objects
+  // Therefore, their initial value by default 'null'
+}
