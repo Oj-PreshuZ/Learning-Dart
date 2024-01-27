4821212f8f4f19922b844ea26e2daaed7ce83834
Ojie Precious <peterjohn56464@gmail.com>
2024-01-27 12:46:43 +0100

    if else

--- bin/example4.dart
+++ bin/example4.dart
@@ -1,5 +1,16 @@
 void main() {
   // final keyword
+  final cityName = "Lagos";
+  // cityName = "Lome";
+
+  final String countryName = 'Togo';
 
   // const keyword
+  const PI = 3.14;
+  const double gravity = 9.8;
+}
+
+class Circle {
+  final color = 'Red';
+  static const PI = 3.14;
 }
---
+++ bin/example5.dart
@@ -0,0 +1,21 @@
+void main() {
+  // IF and ELSE Statements
+  var salary = 10000;
+  if (salary > 20000) {
+    print("You Have Being Promoted!");
+  } else {
+    print('Work Harder');
+  }
+
+  // IF ELSE IF Ladder Statements
+  var marks = 70;
+  if (marks >= 90 && marks < 100) {
+    print('A+');
+  } else if (marks >= 80 && marks < 90) {
+    print('A+');
+  } else if (marks >= 70 && marks < 80) {
+    print('B');
+  } else if (marks < 30) {
+    print('D');
+  }
+}
