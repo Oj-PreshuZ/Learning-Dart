c2fe2f51232e45c1e1b8f3438604a57cd4fa428a
Ojie Precious <peterjohn56464@gmail.com>
2024-01-30 19:35:41 +0100

    continue-loop

---
+++ bin/example13.dart
@@ -0,0 +1,12 @@
+void main() {
+  outerLoop:
+  for (int i = 1; i <= 3; i++) {
+    innerLoop:
+    for (int j = 1; j <= 3; j++) {
+      if (i == 2 && j == 2) {
+        continue innerLoop;
+      }
+      print("$i $j");
+    }
+  }
+}
