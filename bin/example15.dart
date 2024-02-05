602329367fc82859eeb061bdac7103951268e618
Ojie Precious <peterjohn56464@gmail.com>
2024-02-06 00:53:07 +0100

    optional-parameters

---
+++ bin/example15.dart
@@ -0,0 +1,20 @@
+void main() {
+  printCities("Tokyo", "Jalingo", "Paris");
+
+  print('');
+  printCountries("Japan");
+}
+
+// Required Parameters
+void printCities(String name1, String name2, String name3) {
+  print('Name 1 is $name1');
+  print('Name 2 is $name2');
+  print('Name 3 is $name3');
+}
+
+// Optional Positional Parameters
+void printCountries(String name1, [String name2 = "", String name3 = ""]) {
+  print('Name 1 is $name1');
+  print('Name 2 is $name2');
+  print('Name 3 is $name3');
+}
