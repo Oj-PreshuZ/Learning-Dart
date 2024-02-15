53d6dc9d708716a7e3acba4e3783a406d5155e17
Ojie Precious <peterjohn56464@gmail.com>
2024-02-15 20:09:13 +0100

    exception-handling

--- bin/example17.dart
+++ bin/example17.dart
@@ -23,4 +23,29 @@ void main() {
     print('The exception thrown is $e');
     print('STACK TRACE \n $s');
   }
+
+  print('CASE 4');
+  try {
+    int result = 12 ~/ 3;
+    print('The result is $result');
+  } catch (e) {
+    print('The exception thrown is $e');
+  } finally {
+    print('ThIS IS A Finally clause');
+  }
+
+  print('CASE 5');
+  try {
+    depositMoney(-200);
+  } catch (e) {
+    print(e);
+  }
+}
+
+class DepositException implements Exception {}
+
+void depositMoney(int amount) {
+  if (amount < 0) {
+    throw DepositException();
+  }
 }
